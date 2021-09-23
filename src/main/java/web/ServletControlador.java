4898f643cb8454f62fe4652fd330f18f6847d5cb
static-x03 <static-x03@hotmail.com>
2021-09-23 16:14:31 -0500

    proyecto servlet y jsp

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -151,7 +151,7 @@ public class ServletControlador extends HttpServlet {
 				int registrosActualizados = clienteImp.update(cliente);
 				System.out.println("registros Actualizados : " + registrosActualizados);
 
-				// redireccion accion por default metodo get render
+				// redireccion accion por default metodo render
 				this.accionDefault(request, response);
 	}
 	
